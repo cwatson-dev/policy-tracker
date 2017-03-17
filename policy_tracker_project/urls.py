67ee74e959b3c931b38c1cb73cc60d91548e9fc7
2109600a <2109600a@student.gla.ac.uk>
2017-03-17 14:32:42 +0000

    register

--- policy_tracker_project/urls.py
+++ policy_tracker_project/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from django.conf.urls.static import static
+from policy_tracker_app import views
 
 urlpatterns = [
     url(r'^', include('policy_tracker_app.urls')),
+    url(r'^register/$',views.register,name='register'),
     url(r'^admin/', admin.site.urls),
     url(r'^oauth/', include("social_django.urls", namespace="social")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
