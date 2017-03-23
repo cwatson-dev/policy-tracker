c657b5a9349b7e1858c27cd237785c1780cfd4b7
Christopher Atkinson <2109600a@student.gla.ac.uk>
2017-03-23 18:45:46 +0000

    forms commented and updates bottom of homepage text with FAQs link

--- policy_tracker_app/forms.py
+++ policy_tracker_app/forms.py
@@ -3,31 +3,37 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from policy_tracker_app.models import Country, Policy, Category, UserProfile
 
+# basic user form
 class UserForm(forms.ModelForm):
+    # ensure password is hidden when typed
     password = forms.CharField(widget=forms.PasswordInput())
+    # make user repeat password to test whether both are the same
     confirm_password = forms.CharField(widget=forms.PasswordInput(), help_text="Please retype password")
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
 
+    # ensure user has entered their password correctly by confirming both are the same
     def clean(self):
         password1 = self.cleaned_data.get('password')
         password2 = self.cleaned_data.get('confirm_password')
-
+        # if passwords are not the same return error
         if password1 != password2:
             raise forms.ValidationError('Passwords do not match.', code='invalid')
-
+        # validate passwords to ensure they are not simple
         password1 = validate_password(password1)
         password2 = validate_password(password2)
 
         return self.cleaned_data
 
+# user profile form extension with website and picture
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
 
+# add country form
 class CountryForm(forms.ModelForm):
     name = forms.CharField(max_length=64, help_text="Please enter the country name.")
     inPower = forms.CharField(max_length=64, help_text="Please enter the governing body/ruler.")
